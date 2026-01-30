6fe7a49dae32d8716e3b684e401b7b6aa72334b3
himareddygundla <himareddygundla@gmail.com>
2026-01-30 15:33:54 +0530

    fourth day commit

---
+++ day4/exception_handling/program1.py
@@ -0,0 +1,18 @@
+# print("step1")
+# a=10
+# b=20
+# result=a/b
+# print("step2")
+# print("Result is:",result)
+
+try:
+    print("step1")
+    a=int(input("Enter numerator: "))
+    b=int(input("Enter denominator: "))
+    result=a/b
+    print("step2")
+    print("Result is:",result)
+except Exception as e:
+    print("Error:Division by zero is not allowed")
+finally:
+    print("Execution completed")
